26d3bda61a804e207595852d6e4960f28b22c1f7
Matthew Lee <mlee212@wch136-09.cs.ucr.edu>
2020-04-08 15:37:37 -0700

    Initializing repo

---
+++ Lab2_introToAVR/source/main.c
@@ -0,0 +1,23 @@
+/*	Author: mlee212
+ *  Partner(s) Name: Matthew Lee
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+
+    /* Insert your solution below */
+    while (1) {
+
+    }
+    return 1;
+}
